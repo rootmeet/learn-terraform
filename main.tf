66a511977a1809fec5967acf6cd1bbd5db586239
Root Meet <rootmeet@gmail.com>
2023-12-16 11:07:27 -0700

    nginx docker resource added

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ resource "local_file" "meri_file"{
     filename = "nibba.txt"
     content="where is nibbi?"
 }
+
+resource "docker_image" "meri_nginx_image" {
+  name = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "my_nginx_container" {
+    image = docker_image.meri_nginx_image.image_id
+    name = "nginx-container"
+    ports{
+        internal = 80
+        external = 80
+    }  
+}
