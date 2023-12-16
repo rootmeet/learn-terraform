f80530a1830a2163cb357363095441f7ea427313
Root Meet <rootmeet@gmail.com>
2023-12-16 11:44:33 -0700

    aws instance added

--- infra.tf
+++ infra.tf
@@ -5,3 +5,13 @@ resource "aws_s3_bucket" "meri_rootmeet_s3_bucket" {
         environment = "dev"
       }
 }
+
+resource "aws_instance" "meri_ec2_instance" {
+    ami = "ami-0a07b0077b66673f1"
+    instance_type = "t2.micro"
+    key_name = "my-ssh-key"
+    tags = {
+        name = "meri-ec2-instance"
+        environment = "dev"
+      }  
+}
