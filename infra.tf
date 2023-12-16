33b4f91f0154e8d65b34adde69a80dd43a35b66c
Root Meet <rootmeet@gmail.com>
2023-12-16 11:34:43 -0700

    bucket name changed

--- infra.tf
+++ infra.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "meri_rootmeet_s3_bucket" {
-      bucket = "meri_rootmeet_s3_bucket"
+      bucket = "meri-rootmeet-s3-bucket"
       tags = {
-        name = "meri_rootmeet_s3_bucket"
+        name = "meri-rootmeet-s3-bucket"
         environment = "dev"
       }
 }
